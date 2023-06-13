2e4c956d22ad1492926d14ccad6527ce61a50865
hydee <hydee0209@hotmail.com>
2023-06-13 17:26:03 +0800

    Initial Commit

---
+++ BMO/BMOApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMOApp.swift
+//  BMO
+//
+//  Created by Hydee Chen on 2023/6/13.
+//
+
+import SwiftUI
+
+@main
+struct BMOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
